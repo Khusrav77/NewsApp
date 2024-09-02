16813fc4830c261e0c4ca00f545e3072e09e3ed9
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-02 13:26:45 +0300

    added git ignor and created project

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Khusrav Safiev on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
