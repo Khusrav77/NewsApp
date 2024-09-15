b08efbebed07d00d68f5191216fbe009158fcc68
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-16 00:03:18 +0300

    added MainImageView

---
+++ NewsApp/NewsApp/Views/DetailScreen/GradientAvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  GradientAvatarView.swift
+//  NewsApp
+//
+//  Created by Khusrav Safiev on 9/15/24.
+//
+
+import SwiftUI
+
+struct GradientAvatarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientAvatarView()
+}
---
+++ NewsApp/NewsApp/Views/DetailScreen/MainImageView.swift
@@ -0,0 +1,18 @@
+//
+//  MainImageView.swift
+//  NewsApp
+//
+//  Created by Khusrav Safiev on 9/15/24.
+//
+
+import SwiftUI
+
+struct MainImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainImageView()
+}
