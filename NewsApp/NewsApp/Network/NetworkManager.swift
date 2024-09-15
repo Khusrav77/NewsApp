83f79344554e2785e2dffeeaf312ba5367d59e9b
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-15 16:51:02 +0300

    update some code

--- NewsApp/NewsApp/Network/NetworkManager.swift
+++ NewsApp/NewsApp/Network/NetworkManager.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 
+// MARK: - NetworkManagerProtocol
+protocol NetworkManagerProtocol {
+    func getNews() async throws -> News
+}
 
-final class NetworkManager {
+// MARK: - NetworkManager
+final class NetworkManager: NetworkManagerProtocol {
     
     // MARK: - Properties
     static let shared = NetworkManager()
+    private let url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=5dd079f0bc5342bba152602a26d4387a"
     private let urlNews = "https://newsapi.org/v2/everything?q=apple&from=2024-09-01&to=2024-09-01&sortBy=popularity&apiKey=5dd079f0bc5342bba152602a26d4387a"
     private let decoder = JSONDecoder()
    
@@ -20,14 +26,14 @@ final class NetworkManager {
         decoder.dateDecodingStrategy = .iso8601
     }
     
-    // Methods
+    // MARK: - Methods
     func getNews() async throws -> News {
         guard let url = URL(string: urlNews) else { throw NetworkError.invalidURL }
         let (data, response) = try await URLSession.shared.data(from: url)
         
         guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw NetworkError.invalidResponse }
         do {
-            return try decoder.decode(News.self, from: data)
+            return  try decoder.decode(News.self, from: data)
         } catch  {
             throw NetworkError.invalidData
         }
