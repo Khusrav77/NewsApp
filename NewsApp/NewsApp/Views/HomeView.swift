e72cef51e79faa472331e4a7203cf3e3e0366224
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-15 16:51:59 +0300

    added topArticleView

--- NewsApp/NewsApp/Views/HomeView.swift
+++ NewsApp/NewsApp/Views/HomeView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject var vm = ViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                Text("Top News")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.top)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach (vm.news, id: \.url) { article in
+                            TopArticleView(article: article)
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
+            .background(.secondary.opacity(0.2))
+        }
     }
 }
 
 #Preview {
     HomeView()
 }
+
+
+
+
