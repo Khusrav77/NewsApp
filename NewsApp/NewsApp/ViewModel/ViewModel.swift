bb367975d05f7cb0e1e2e57318e930ececf24175
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-15 20:30:44 +0300

    added TeslsNews and updet some code

--- NewsApp/NewsApp/ViewModel/ViewModel.swift
+++ NewsApp/NewsApp/ViewModel/ViewModel.swift
@@ -11,19 +11,34 @@ import Foundation
 final class ViewModel: ObservableObject {
     
     // MARK: - Properties
-    @Published var news: [Article] = []
+    @Published var AppleNews: [Article] = []
+    @Published var TeslaNews: [Article] = []
     
     // MARK: - Initializet
     init() {
-        fetchNews()
+        fetchTopNews()
+        fetchBottomNews()
     }
     
     // MARK: - Mtheds
-    func fetchNews() {
+    func fetchTopNews() {
         Task {
             do {
-                let articles = try await NetworkManager.shared.getNews()
-                news = articles.articles
+                let articles = try await NetworkManager.shared.getNews(urlNews: URLConstants.AppleNews)
+                AppleNews = articles.articles
+            } catch  {
+                if let error = error as? NetworkError {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    func fetchBottomNews() {
+        Task {
+            do {
+                let articles = try await NetworkManager.shared.getNews(urlNews: URLConstants.TeslaNews)
+                TeslaNews = articles.articles
             } catch  {
                 if let error = error as? NetworkError {
                     print(error)
