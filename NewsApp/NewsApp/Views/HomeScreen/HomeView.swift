117bf7a74e47c79f899e48edfbe905e3a765e394
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-15 21:23:29 +0300

    update some code

--- NewsApp/NewsApp/Views/HomeScreen/HomeView.swift
+++ NewsApp/NewsApp/Views/HomeScreen/HomeView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    // MARK: - Properties
     @StateObject var vm = ViewModel()
     
+    // MARK: - Body
     var body: some View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
                 
+                // MARK: - Apple News
                 Text("Apple News")
                     .font(.title)
                     .fontWeight(.bold)
@@ -22,14 +26,16 @@ struct HomeView: View {
                     .padding(.top)
                 
                 ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
+                    HStack(spacing: 12) {
                         ForEach (vm.AppleNews, id: \.url) { article in
                             TopArticleView(article: article)
                         }
                     }
                     .padding(.horizontal)
                 }
+                .shadow(color: .black.opacity(0.1), radius: 8, x: 5, y: 8)
                 
+                // MARK: - Tesla News
                 Text("Tesla News")
                     .font(.title)
                     .fontWeight(.bold)
@@ -38,15 +44,20 @@ struct HomeView: View {
                     .padding(.top)
                 
                 ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
+                    HStack(spacing: 12) {
                         ForEach (vm.TeslaNews, id: \.url) { article in
                             BottomNewsView(article: article)
                         }
                     }
                     .padding(.horizontal)
                 }
+                .shadow(color: .black.opacity(0.1), radius: 8, x: 5, y: 8)
             }
-            .background(.secondary.opacity(0.2))
+            .background(.secondary.opacity(0.3))
+            .refreshable {
+                vm.fetchAppleNews()
+                vm.fetchTeslaNews()
+            }
         }
     }
 }
