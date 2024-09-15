ff1cea8789a4fae0ffe489e496269166ac918a35
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-15 20:33:06 +0300

    added buttomNews

--- NewsApp/NewsApp/Views/SubViews/BottomNewsView.swift
+++ NewsApp/NewsApp/Views/SubViews/BottomNewsView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct BottomNewsView: View {
+    let article: Article
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let url = article.urlToImage, let image = URL(string: url) {
+                AsyncImage(url: image) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 120, height: 120)
+                            .clipShape(.rect(cornerRadius: 10))
+                    } else {
+                        ZStack {
+                            Rectangle()
+                                .foregroundStyle(.secondary.opacity(0.3))
+                                .frame(width: 120, height: 120)
+                                .clipShape(.rect(cornerRadius: 10))
+                            
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 50)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+            }
+            
+            
+            VStack(alignment: .leading) {
+                
+                Text(article.title)
+                    .titleFont()
+                
+                Spacer()
+                
+                Text(article.publishedAt.convertDate())
+                    .descriptionFont()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+        }
+        .frame(width: 120, height: 240)
+        .padding(10)
+        .background(.background)
+        .clipShape(.rect(cornerRadius: 10))
     }
 }
 
-#Preview {
-    BottomNewsView()
-}
+
