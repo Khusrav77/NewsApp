9324959832374f850b4b369c5cf08594d3842465
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-16 14:21:09 +0300

    bugfixed

--- NewsApp/NewsApp/Network/URLConstants.swift
+++ NewsApp/NewsApp/Network/URLConstants.swift
@@ -9,7 +9,7 @@ struct URLConstants{
     
     static let AppleNews = "https://newsapi.org/v2/everything?q=apple&from=2024-09-01&to=2024-09-01&sortBy=popularity&apiKey=5dd079f0bc5342bba152602a26d4387a"
 
-    static let TeslaNews = "https://newsapi.org/v2/everything?q=tesla&from=2024-08-15&sortBy=publishedAt&apiKey=5dd079f0bc5342bba152602a26d4387a"
+    static let TeslaNews = "https://newsapi.org/v2/everything?q=tesla&from=2024-08-16&sortBy=publishedAt&apiKey=5dd079f0bc5342bba152602a26d4387a"
 
 }
 
